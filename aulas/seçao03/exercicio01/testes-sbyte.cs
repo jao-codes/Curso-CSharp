c13df26dcee21f617f4947263d40c0a913d8e731
João Ritzke <joaopedroritzke28@gmail.com>
2025-03-03 22:51:27 -0300

    aula de tipos de dados

---
+++ aulas/seçao03/exercicio01/testes-sbyte.cs
@@ -0,0 +1,19 @@
+using System; 
+
+namespace Course {
+    class Program {
+        static void Main(string[] args) {
+
+            int n1 = int.MinValue;
+            int n2 = int.MaxValue;
+            sbyte n3 = sbyte.MinValue;
+            decimal n4 = decimal.MaxValue;
+
+            System.Console.WriteLine(n1);
+            System.Console.WriteLine(n2);
+            System.Console.WriteLine(n3);
+            System.Console.WriteLine(n4);
+
+        }
+    }
+}
